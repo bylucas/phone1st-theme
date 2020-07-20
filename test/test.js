115a2c9b32221306ae58ad9a7d74869032382a4c
Howard Lucas <h@bylucas.co.uk>
2020-07-20 17:16:25 +0100

    july20

--- test/test.js
+++ test/test.js
@@ -10,7 +10,14 @@ function scrollFunction() {
   }
 }
 
+var timeOut;
+
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0)
+  {
+  	window.scrollBy(0,-50);
+ 	timeOut=setTimeout('topFunction()', 10);
+  }
+
+  else clearTimeout(timeout);
 }
