e552bc7a920a50de89315aebd7888da4c6d213e5
Howard Lucas <how5360@gmail.com>
2024-01-16 12:51:45 +0000

    Update firebase-config.js

--- test/firebase/firebase-config.js
+++ test/firebase/firebase-config.js
@@ -3,13 +3,7 @@ import { getFirestore } from "firebase/firestore"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBgOheePCJqT_ivlXs_EagQ3wQPGIMpJUg",
-  authDomain: "howardlucas-7fcb0.firebaseapp.com",
-  projectId: "howardlucas-7fcb0",
-  storageBucket: "howardlucas-7fcb0.appspot.com",
-  messagingSenderId: "945745527827",
-  appId: "1:945745527827:web:3e282534ecdd22c66eabe6",
-  measurementId: "G-QEE9Y648VB"
+ 
 }
 
 // init firebase
